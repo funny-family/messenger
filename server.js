a895a8d1156e572445144fa4ae3fa08fbc268584
funny family <maksvadik228@gmail.com>
2020-06-03 18:46:05 +0700

    completed server config

--- server.js
+++ server.js
@@ -2,10 +2,25 @@ require('dotenv').config();
 const config = require('config');
 const http = require('http');
 const Koa = require('koa');
+const logger = require('koa-logger');
+const responseTime = require('koa-response-time');
 
 const app = new Koa();
 const server = http.createServer(app.callback());
 
+global.__PROD__ = 'production';
+global.__DEV__ = 'development';
+
+app.proxy = false;
+
+if (global.__DEV__ === process.env.NODE_ENV) {
+  app.use(logger());
+}
+
+app.use(responseTime({ hrtime: false }));
+
 server.listen(config.port, () => {
   console.log(`Server running at: http://localhost:${config.port}/`);
+  console.log(global.__DEV__);
+  console.log(global.__DEV__ === process.env.NODE_ENV);
 });
