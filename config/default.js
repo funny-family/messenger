cf031597a7f4e9914ff2a115bc07677589c5cdfc
funny family <maksvadik228@gmail.com>
2020-06-04 16:11:17 +0700

    added mongoose config

--- config/default.js
+++ config/default.js
@@ -1,3 +1,11 @@
 module.exports = {
-  //
+  mongoose: {
+    uri: 'mongodb://localhost:27017',
+    options: {
+      dbName: 'messanger',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    }
+  }
 };
