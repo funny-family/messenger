8e4e281a663ff62a4d13569e326973dc99988bc5
funny family <maksvadik228@gmail.com>
2020-06-11 18:36:49 +0700

    added passwordCheck and toJSON methods

--- modules/auth/router.js
+++ modules/auth/router.js
@@ -1,15 +1,17 @@
 const Router = require('koa-router');
 const User = require('../user/models/User');
+const bodyParser = require('../../middlewares/body-parser');
 
 const auth = new Router({
   prefix: '/auth'
 });
 
 auth
-  .post('/signup', async ctx => {
+  .post('/signup', bodyParser, async ctx => {
     const userData = ctx.request.body;
     const newUser = new User(userData);
-    console.log(newUser);
+    newUser.validation_token = 'uuid' + Math.random();
+    ctx.body = newUser;
     // newUser.save();
   })
   .post('/signin', async ctx => {
