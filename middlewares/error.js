c05c60a3ed0b781668417c41fff10b5c84792141
funny family <maksvadik228@gmail.com>
2020-06-12 16:05:23 +0700

    changed condition when server should log error

--- middlewares/error.js
+++ middlewares/error.js
@@ -3,7 +3,7 @@ module.exports = async (ctx, next) => {
     await next();
   } catch (err) {
     ctx.log.error(err);
-    if (global.__DEV__) return console.error(err);
+    if (global.__DEV__ === process.env.NODE_ENV) console.error(err);
     if (ctx.status > 500) {
       ctx.status = 500;
       ctx.body = 'Server error';
