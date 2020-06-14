5890eb80c0cdc5b10d5568bb9edd799f9d440f48
funny family <maksvadik228@gmail.com>
2020-06-15 00:00:57 +0700

    added local strategy

--- modules/auth/strategies/local.js
+++ modules/auth/strategies/local.js
@@ -1,2 +1,15 @@
-// const passportLocal = require('passport-local').Strategy;
-// const userModel = require('../../user/user-model');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../user/models/User');
+
+module.exports = new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  session: false
+}, function (email, password, done) {
+  User.findOne({ email }, function (err, user) {
+    if (err) return done(err);
+    if (!user) return done(null, false, 'User not found!');
+    if (!user.passwordCheck(password)) return done(null, false, 'Incorrect password!');
+    return done(null, false);
+  });
+});
