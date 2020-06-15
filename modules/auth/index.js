119fda49638e6b5bdfaf82294f3321ba927bbef6
funny family <maksvadik228@gmail.com>
2020-06-15 21:48:05 +0700

    added passport initialize method

--- modules/auth/index.js
+++ modules/auth/index.js
@@ -1,6 +1,8 @@
 const router = require('./router');
+const passport = require('./middlewares/passport');
 
 module.exports = app => {
+  app.use(passport.initialize());
   router.forEach(route => {
     app.use(route.routes());
   });
