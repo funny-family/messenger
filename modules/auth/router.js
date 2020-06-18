9402b18f97e3d39b416c66d75b4ff8034136618c
funny family <maksvadik228@gmail.com>
2020-06-19 00:38:32 +0700

    added authenticator to routes

--- modules/auth/router.js
+++ modules/auth/router.js
@@ -1,28 +1,28 @@
 const Router = require('koa-router');
-const User = require('../user/models/User');
 const bodyParser = require('../../middlewares/body-parser');
 const passport = require('./middlewares/passport');
-const tokenContoller = require('./controllers/tokens');
+const authenticator = require('./controllers/authenticator');
 
 const auth = new Router({
   prefix: '/auth'
 });
 
-auth
-  .post('/signup', bodyParser, async ctx => {
-    const userData = ctx.request.body;
-    const newUser = new User(userData);
-    newUser.validation_token = 'uuid' + Math.random();
-    await newUser.save();
-    ctx.body = newUser;
-  })
-  .post('/signin', bodyParser, passport.authenticate('local', { session: false }), async ctx => {
-    const tokens = tokenContoller.createAccessAndRefreshTokens(ctx.state.user);
-    await tokenContoller.setCookiesAndTokens(ctx, tokens);
-    ctx.body = tokens;
-  })
-  .post('/signout', async ctx => {
-    ctx.body = 'signout';
-  });
+auth.post(
+  '/signup',
+  bodyParser,
+  authenticator.signup
+);
+auth.post(
+  '/signin',
+  bodyParser,
+  passport.authenticate('local', { session: false }),
+  authenticator.signin
+);
+auth.post(
+  '/signout',
+  bodyParser,
+  passport.authenticate('jwt', { session: false }),
+  authenticator.signout
+);
 
 module.exports = [auth];
