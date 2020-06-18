360650444fbee9dfde593f97e4e651ac3c0c79b7
funny family <maksvadik228@gmail.com>
2020-06-18 18:03:26 +0700

    fixed Unauthorized error

--- modules/auth/strategies/local.js
+++ modules/auth/strategies/local.js
@@ -7,12 +7,16 @@ module.exports = new LocalStrategy({
   session: false
 }, function (email, password, done) {
   User.findOne({ email }, function (err, user) {
-    if (err) return done(err);
-
-    if (!user) return done(null, false, 'User not found!');
-
-    if (!user.checkPassword(password)) return done(null, false, 'Incorrect password!');
-
-    return done(null, false);
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      console.log(1);
+      return done(null, false, 'User not found!');
+    }
+    if (!user.checkPassword(password)) {
+      return done(null, false, 'Incorrect password!');
+    }
+    return done(null, user);
   });
 });
