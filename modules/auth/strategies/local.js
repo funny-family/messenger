c427ee67dc3883d81bd8d95c296331202fa73778
funny family <maksvadik228@gmail.com>
2020-06-29 00:43:07 +0700

    added signin route error handler

--- modules/auth/strategies/local.js
+++ modules/auth/strategies/local.js
@@ -1,19 +1,36 @@
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../../user/models/User');
 
+const emailPath = 'email';
+const passwordPath = 'password';
+const errorObject = {
+  errors: {
+    email: {
+      properties: {
+        path: '',
+        message: ''
+      }
+    }
+  }
+};
+
 module.exports = new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password',
+  usernameField: emailPath,
+  passwordField: passwordPath,
   session: false
 }, async function (email, password, done) {
   try {
     const user = await User.findOne({ email: String(email) });
 
     if (!user) {
-      return done(null, false, { message: 'User not found!' });
+      errorObject.errors.email.properties.message = 'User not found!';
+      errorObject.errors.email.properties.path = emailPath;
+      throw errorObject;
     }
     if (!user.checkPassword(password)) {
-      return done(null, false, { message: 'Incorrect password!' });
+      errorObject.errors.email.properties.message = 'Incorrect password!';
+      errorObject.errors.email.properties.path = passwordPath;
+      throw errorObject;
     }
     return done(null, user);
   } catch (err) {
