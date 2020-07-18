5b9d541fa6a317bf6f9a2c8934c084d20490a8f6
funny family <maksvadik228@gmail.com>
2020-07-18 13:08:31 +0700

    added body parser error handler and enabled types to parse

--- middlewares/body-parser.js
+++ middlewares/body-parser.js
@@ -2,5 +2,9 @@ const bodyParser = require('koa-bodyparser');
 
 module.exports = bodyParser({ // application/x-www-form-urlencoded and application/json
   formLimit: '56kb',
-  jsonLimit: '1mb'
+  jsonLimit: '1mb',
+  enableTypes: ['json', 'form'],
+  onerror: (ctx) => {
+    ctx.throw('Body parse error.', 422);
+  }
 });
