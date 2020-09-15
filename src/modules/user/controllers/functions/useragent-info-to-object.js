96b2c7fb8b90774fbf56c884c4b6a506e8ef4f80
funny-family <maksvadik228@gmail.com>
2020-09-15 18:38:49 +0700

    added start variables for useragent object

--- src/modules/user/controllers/functions/useragent-info-to-object.js
+++ src/modules/user/controllers/functions/useragent-info-to-object.js
@@ -8,6 +8,23 @@ exports.useragentInfoToObject = async function (ctx) {
   //   console.log(match);
   // });
 
-  const wordsStartWhithIs = userAgentStringInfo.match(/is+\w{0,}.\s(false|true)/gm);
-  console.log(wordsStartWhithIs);
+  // const wordsStartWhithIs = userAgentStringInfo.match(/is+\w{0,}.\s(false|true)/gm);
+  // console.log(wordsStartWhithIs);
+
+  const wordsStartWhithIs = userAgentStringInfo.match(/is+\w{0,}/gm);
+  const wordsStartWhithIsValue = userAgentStringInfo.match(/false|true/gm);
+
+  // console.log(Object.keys(wordsStartWhithIs).map((key) => [Number(key), wordsStartWhithIs[key]]));
+  const container = [];
+  for (let i = 0; i < container.length; i++) {
+    container.pop(wordsStartWhithIs[i]);
+  }
+  console.log(container);
+
+  const boolenUserAgentValues = {};
+  wordsStartWhithIs.foreach((key, value) => {
+    boolenUserAgentValues[key] = wordsStartWhithIsValue[value];
+  });
+
+  console.log(boolenUserAgentValues);
 };
