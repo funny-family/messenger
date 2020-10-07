660d5a18952de93ff586a0c5c92da2d13cf03367
funny-family <maksvadik228@gmail.com>
2020-10-07 14:18:59 +0700

    added log cleaner

---
+++ src/tools/logs-cleaner.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const fileDirectory = path.join(__dirname, '/../logs/errors.log');
+
+fs.truncate(fileDirectory, 0, (err, bytes) => {
+  if (err) {
+    console.log('\x1b[31m', `Cannot clean file in "${fileDirectory}" directory.`);
+    throw err;
+  }
+
+  console.log('File is cleaned!');
+});
