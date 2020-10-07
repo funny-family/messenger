c67d92c98f182bf801294de4265ce65433888f6c
funny-family <maksvadik228@gmail.com>
2020-10-07 14:25:24 +0700

    removed extra functon argument

--- src/tools/logs-cleaner.js
+++ src/tools/logs-cleaner.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const fileDirectory = path.join(__dirname, '/../logs/errors.log');
 
-fs.truncate(fileDirectory, 0, (err, bytes) => {
+fs.truncate(fileDirectory, 0, (err) => {
   if (err) {
     console.log('\x1b[31m', `Cannot clean file in "${fileDirectory}" directory.`);
     throw err;
