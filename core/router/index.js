027c04ee740f18801f1080414a04344721cb2759
funny-family <maksvadik228@gmail.com>
2020-10-09 18:56:57 +0700

    fix

--- core/router/index.js
+++ core/router/index.js
@@ -1,7 +1,36 @@
-module.exports = class Router {
+const Router = class {
   constructor(controllerFunction) {
     this.controllerFunction = controllerFunction;
   }
 
   static createRoute() {}
 };
+
+exports.Router = Router;
+
+// https://stackoverflow.com/questions/7764536/pass-object-to-javascript-function
+
+// exports.combineRoutes = (app, routes, middlewares) => {
+//   // app.use(passport.initialize());
+//   routes.forEach((route) => {
+//     app.use(route.routes());
+//   });
+// };
+
+exports.combineRoutes = (args) => { // applicationInstance, routes, middlewares
+  args.middlewares.forEach((middleware) => {
+    args.app.use(middleware);
+  });
+
+  args.routes.forEach((route) => {
+    args.app.use(route.routes());
+  });
+};
+
+// combineRoutes({
+//   app,
+//   routes,
+//   middlewares: [
+//     app.use(passport.initialize());
+//   ]
+// });
