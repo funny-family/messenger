b74e685772fc8c93f2498158c98291cbe1bc762e
funny family <maksvadik228@gmail.com>
2020-10-09 19:46:02 +0700

    renamed router in auth module to routes

--- src/modules/auth/index.js
+++ src/modules/auth/index.js
@@ -1,9 +1,9 @@
 const passport = require('@/middlewares/passport');
-const router = require('./router');
+const routes = require('./routes');
 
 module.exports = app => {
   app.use(passport.initialize());
-  router.forEach(route => {
-    app.use(route.routes());
+  routes.map((route) => {
+    return app.use(route.routes());
   });
 };
