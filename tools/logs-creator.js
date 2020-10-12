4de617468172057718f6572c63bd0d7017536eca
funny family <maksvadik228@gmail.com>
2020-10-12 23:31:18 +0700

    added file checks for logs creator

--- tools/logs-creator.js
+++ tools/logs-creator.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
+const path = require('path');
 
 const fileName = 'errors.log';
+const fileDirectory = path.join(__dirname, `/../src/logs/${fileName}`);
 
-fs.watchFile('../src/logs/errors.log', '', (err) => {
-  if (err) {
-    console.log('\x1b[31m', `Cannot create file ${fileName}!`);
-    throw err;
-  }
+if (fs.existsSync(fileDirectory)) {
+  console.log(`${fileName} is already exists!`);
+} else {
+  fs.writeFile(fileDirectory, '', (err) => {
+    if (err) {
+      console.log('\x1b[31m', `Cannot create file ${fileName}!`);
+      throw err;
+    }
 
-  console.log('Results Received');
-});
+    console.log('Log file is created!');
+  });
+}
