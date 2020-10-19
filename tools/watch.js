f178cf8385dba23005fd4d1ea6bb1b9b7cc2d5d9
funny family <maksvadik228@gmail.com>
2020-10-19 11:08:00 +0700

    feat(tool): add watch tool
    Watch tool serves to get all modules names in folder and then connect to main app file.

---
+++ tools/watch.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const modulesDirectory = path.join(__dirname, '../src/modules');
+
+fs.readdir(filesDirectory, (err, files) => {
+  if (err) {
+    console.log('Some kind of error((((');
+    throw err;
+  }
+
+  files.forEach((file) => {
+    console.log(file);
+  });
+});
+
+fs.readdirSync(modulesDirectory).forEach(file => {
+  console.log(file);
+});
+
+// https://stackoverflow.com/questions/2727167/how-do-you-get-a-list-of-the-names-of-all-files-present-in-a-directory-in-node-j
+// https://medium.com/stackfame/get-list-of-all-files-in-a-directory-in-node-js-befd31677ec5
+
+// https://stackoverflow.com/questions/13695046/watch-a-folder-for-changes-using-node-js-and-print-file-paths-when-they-are-cha
+// https://bezkoder.com/node-js-watch-folder-changes/
