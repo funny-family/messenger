d92d38db86285fe13d3f67be9e04721818e55033
funny family <maksvadik228@gmail.com>
2020-10-20 21:26:49 +0700

    feat(modules): add index file
    Index file in module servce to connect all Routers.

---
+++ src/modules/index.js
@@ -0,0 +1,8 @@
+// const { connectRouters } = require('../../core/router');
+
+// const folderPath = '../models';
+
+// module.exports = (app) => connectRouters({
+//   appInstance: app,
+//   folderPath
+// });
