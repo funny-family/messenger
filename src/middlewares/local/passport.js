893387a7187ef787517e5fdb42ea3472452b7989
funny family <maksvadik228@gmail.com>
2020-10-30 23:13:12 +0700

    refactor(middleware): change passport function
    Reinitialize "initialize" and "authenticate" passport function.

--- src/middlewares/local/passport.js
+++ src/middlewares/local/passport.js
@@ -3,4 +3,31 @@ const passport = require('koa-passport');
 passport.use('local', require('@/strategies/local'));
 passport.use('jwt', require('@/strategies/jwt'));
 
-module.exports = passport;
+module.exports = {
+  initialize: () => {
+    return passport.initialize();
+  },
+  authenticate: (strategy, options) => {
+    return async (ctx, next) => {
+      await passport.authenticate(strategy, { session: false, ...options }, async (err, user, info) => {
+        if (err) throw ctx.throw(err);
+
+        if (!user) {
+          return ctx.throw(
+            strategy === 'local' ?
+              400 : 401,
+            info ? (info.field && !Array.isArray(info)) ?
+              JSON.stringify([info]) : info.message ?
+                info.message : null : null
+          );
+        }
+
+        ctx.state.user = user;
+
+        await next();
+      })(ctx, next);
+    };
+  }
+};
+
+// https://github.com/koalex/gs-auth/blob/master/middlewares/passport.js
