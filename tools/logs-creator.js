7288c90a4ceedb8234da04204897698411a3ce2b
funny family <maksvadik228@gmail.com>
2020-11-06 00:22:19 +0700

    style(tools): change log discription in log-creator

--- tools/logs-creator.js
+++ tools/logs-creator.js
@@ -5,14 +5,14 @@ const fileName = 'errors.log';
 const fileDirectory = path.join(__dirname, `../src/logs/${fileName}`);
 
 if (fs.existsSync(fileDirectory)) {
-  console.log(`${fileName} is already exists!`);
+  console.log('\x1b[33m', `${fileName} is already exists!`);
 } else {
   fs.writeFile(fileDirectory, '', (err) => {
     if (err) {
-      console.log('\x1b[31m', `Cannot create file ${fileName}!`);
+      console.log('\x1b[31m', `Cannot create ${fileName} file!`);
       throw err;
     }
 
-    console.log('Log file is created!');
+    console.log('\x1b[36m', `${fileName} file is clear!`);
   });
 }
