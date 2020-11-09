9e645e7b4385f6489e1d3448cf623198d028a804
funny family <maksvadik228@gmail.com>
2020-11-09 12:33:17 +0700

    refactor: added bootstap function in server.js file

--- src/server.js
+++ src/server.js
@@ -5,17 +5,24 @@ const config = require('config');
 const http = require('http');
 const Koa = require('koa');
 
-const app = new Koa();
-const server = http.createServer(app.callback());
+async function bootstrap() {
+  const app = new Koa();
+  const server = http.createServer(app.callback());
 
-app.proxy = config.proxy;
-app.keys = require('./middlewares/global/keygrip');
+  app.proxy = config.proxy;
+  app.keys = require('./middlewares/global/keygrip');
 
-require('./middlewares')(app);
+  require('./middlewares')(app);
+  require('./modules')(app);
 
-require('./modules')(app);
+  await server.listen(config.port);
+}
 
-server.listen(config.port, () => {
-  console.log('\x1b[36m', `Server running at: http://localhost:${config.port}/`);
-  console.log('\x1b[36m', 'Mode:', '\x1b[33m', process.env.NODE_ENV);
-});
+bootstrap()
+  .then(() => {
+    console.log('\x1b[36m', `Server running at: http://localhost:${config.port}/`);
+    console.log('\x1b[36m', 'Mode:', '\x1b[33m', process.env.NODE_ENV);
+  })
+  .catch((error) => {
+    console.error('\x1b[31m', `bootstrap function error: ${error}!`);
+  });
