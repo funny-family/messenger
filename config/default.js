97865e3daa067c85d1b9dd26437d22dc7808fba9
funny family <maksvadik228@gmail.com>
2020-11-10 14:25:19 +0700

    feat(config): add logFile
    Added logFile object to config file.

--- config/default.js
+++ config/default.js
@@ -1,9 +1,15 @@
 const path = require('path');
 
+const logFileName = 'errors.log';
+
 module.exports = {
   staticRoot: path.join(process.cwd(), 'src/static'),
   proxy: false,
   secretOrKey: 'secret',
+  logFile: {
+    name: logFileName,
+    directory: path.join(__dirname, `../src/logs/${logFileName}`)
+  },
   mongoose: {
     uri: 'mongodb://localhost:27017',
     options: {
