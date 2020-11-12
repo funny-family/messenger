ccbb06270963aa91e33d13cc3879d01d10a945a0
funny family <maksvadik228@gmail.com>
2020-11-12 16:49:16 +0700

    refactor(static): add async function to static midleware

--- src/middlewares/global/static.js
+++ src/middlewares/global/static.js
@@ -2,8 +2,13 @@ const config = require('config');
 
 const serve = require('koa-static');
 
-module.exports = serve(config.staticFile.path, {
-  index: config.staticFile.entry || 'index.html',
-  maxage: process.env.NODE_ENV ? 8600000 : 0,
-  gzip: true
-});
+module.exports = async (ctx, next) => {
+  const options = {
+    index: config.staticFile.entry || 'index.html',
+    maxage: process.env.NODE_ENV ? 8600000 : 0,
+    gzip: true,
+    hidden: false
+  };
+
+  await serve(config.staticFile.path, options)(ctx, next);
+};
