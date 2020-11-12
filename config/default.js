1829ff8727c79309beb4681a5187298dc6cf9821
funny family <maksvadik228@gmail.com>
2020-11-12 14:19:36 +0700

    refactor(config): change defalult config file
    Use config builder tools to modify default config file.

--- config/default.js
+++ config/default.js
@@ -1,30 +1,54 @@
 const path = require('path');
 
+const { ObjectConfig, ObjectFactory } = require('./tools/config-builder');
+
 const logFileName = 'errors.log';
 
-module.exports = {
-  staticRoot: path.join(process.cwd(), 'src/static'),
-  proxy: false,
-  secretOrKey: 'secret',
-  logFile: {
-    name: logFileName,
-    directory: path.join(__dirname, `../src/logs/${logFileName}`)
-  },
-  mongoose: {
-    uri: 'mongodb://localhost:27017',
-    options: {
-      dbName: 'messanger',
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
+const configArray = [
+  new ObjectConfig({
+    secretOrKey: 'secret'
+  }),
+  new ObjectConfig({
+    server: {
+      proxy: false
     }
-  },
-  crypto: {
-    iterations: 100,
-    keylen: 100,
-    digest: 'sha512'
-  },
-  jsonwebtoken: {
-    algorithm: 'HS512'
-  }
-};
+  }),
+  new ObjectConfig({
+    staticFile: {
+      path: path.join(process.cwd(), 'src/static')
+    }
+  }),
+  new ObjectConfig({
+    mongoose: {
+      uri: 'mongodb://localhost:27017',
+      options: {
+        dbName: 'messanger',
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+      }
+    }
+  }),
+  new ObjectConfig({
+    crypto: {
+      iterations: 100,
+      keylen: 100,
+      digest: 'sha512'
+    }
+  }),
+  new ObjectConfig({
+    jsonwebtoken: {
+      algorithm: 'HS512'
+    }
+  }),
+  new ObjectConfig({
+    logFile: {
+      name: logFileName,
+      directory: path.join(__dirname, `../src/logs/${logFileName}`)
+    }
+  })
+];
+
+const objectConfig = new ObjectFactory(configArray).run();
+
+module.exports = objectConfig;
