aaa6e487f92409f20d99089907fc6f8fd27dfdd0
funny family <maksvadik228@gmail.com>
2020-11-30 23:33:23 +0700

    style(middleware): change function type in headers middlware
    Replace function on arrow function in headers middlware.

--- src/application/middlewares/global/headers.js
+++ src/application/middlewares/global/headers.js
@@ -1,4 +1,4 @@
-module.exports = async function (ctx, next) {
+module.exports = async (ctx, next) => {
   ctx.set('Access-Control-Allow-Origin', 'http://localhost:8080' || 'http://localhost:8081');
   ctx.set('Access-Control-Allow-Credentials', 'true');
   ctx.set('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
